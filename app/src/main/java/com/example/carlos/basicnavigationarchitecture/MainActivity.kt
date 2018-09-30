7e0cb868d4322a76246355bad9aa3ecb1bf8cdb1
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2018-09-30 15:37:31 +0200

    Added toolbar sync
    The toolbar titles for each fragment is set in nav_graph, label attributes.

--- app/src/main/java/com/example/carlos/basicnavigationarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/carlos/basicnavigationarchitecture/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.carlos.basicnavigationarchitecture
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+
+        setupActionBarWithNavController(findNavController(R.id.host_fragment))
+
+    }
+
+    override fun onSupportNavigateUp() = NavHostFragment.findNavController(host_fragment).navigateUp()
 
 }
